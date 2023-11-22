4ea2a9192f613cd630584d7b4447d16eaffaef4c
Frank <franklaercio@gmail.com>
2023-11-21 22:23:47 -0300

    feat: layout improvement and login fixes

--- app/src/main/java/br/ufrn/edu/market/activities/MainActivity.java
+++ app/src/main/java/br/ufrn/edu/market/activities/MainActivity.java
@@ -6,14 +6,21 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.File;
 
 import br.ufrn.edu.market.R;
 import br.ufrn.edu.market.datasource.DatabaseHelper;
 
 public class MainActivity extends AppCompatActivity {
 
-    DatabaseHelper databaseHelper;
+    private DatabaseHelper databaseHelper;
+    private EditText emailEditText;
+    private EditText passwordEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +29,17 @@ public class MainActivity extends AppCompatActivity {
 
         databaseHelper = new DatabaseHelper(this);
 
+        emailEditText = findViewById(R.id.editTextLogin);
+        passwordEditText = findViewById(R.id.editTextPassword);
+
         SharedPreferences sharedPreferences = getSharedPreferences("app_market_preferences", Context.MODE_PRIVATE);
+
+        File arquivo = new File(this.getFilesDir(), "app_market_preferences");
+
+        if (arquivo.exists()) {
+            Intent intent = new Intent(this, MenuActivity.class);
+            startActivity(intent);
+        }
 
         boolean firstTimeOpen = sharedPreferences.getBoolean("first_time_open", true);
 
@@ -39,7 +56,20 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void authenticate() {
-        Intent intent = new Intent(this, MenuActivity.class);
-        startActivity(intent);
+
+        String email = emailEditText.getText().toString().trim();
+        String password = passwordEditText.getText().toString().trim();
+
+        if(!TextUtils.isEmpty(email) && !TextUtils.isEmpty(email)) {
+            if (email.equals("admin") && password.equals("admin")) {
+                Intent intent = new Intent(this, MenuActivity.class);
+                startActivity(intent);
+            } else {
+                Toast.makeText(this, "Não foi possível autenticar!", Toast.LENGTH_LONG).show();
+            }
+        } else {
+            emailEditText.setError("Campo obrigatório");
+            passwordEditText.setError("Campo obrigatório");
+        }
     }
 }
